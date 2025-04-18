0367871375c38b8b3bb4a41f338890fb26e63ee1
YashChaudhari-WORKING <choudhariyash7890@gmail.com>
2025-04-18 13:06:00 +0530

    fav icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,26 @@ import "@/styles/globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import StickyFooter from "../../components/StickyFooter";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-      <StickyFooter />
-    </div>
+    <>
+      <Head>
+        <link rel="icon" type="image/webp" href="/fav.webp" />
+        <title>
+          iTpreneur &#8211; IT Classes in Pune, IT Course in Pune, IT Training
+          Institute In Pune
+        </title>
+      </Head>
+
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+        <StickyFooter />
+      </div>
+    </>
   );
 }
