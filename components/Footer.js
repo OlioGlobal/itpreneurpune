57de162d2d8bf05bd634472289456b385dfd7713
OlioGlobal <olioclientwebsiteleads@gmail.com>
2026-01-02 19:27:01 +0530

    footer update:

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <div className="bg-[#121c72] py-4 mb-16 sm:mb-0">
         <p className="text-white text-center text-[16px]">
-          Copyright © 2025 iTpreneur.
+          Copyright © {new Date().getFullYear()} iTpreneur.
         </p>
       </div>
     </>
